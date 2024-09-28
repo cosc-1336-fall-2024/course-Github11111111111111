0a236316fd701626df6467517f2df704856cc443
Github11111111111111 <ralph.stjohniii@g.austincc.edu>
2024-09-28 22:53:18 +0000

    test chap 2

--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -3,7 +3,7 @@ import devprocess
 def main():
     value1 = input("enter value1")
     value2 = input("enter value2")
-    result = devprocess.add_numbers(int(value1), int(value2))4
+    result = devprocess.add_numbers(int(value1), int(value2))
     print(result)
 
 main()    
