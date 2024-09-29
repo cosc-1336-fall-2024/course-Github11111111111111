53845e589ef067ef8e53ced4adec95876de3aa14
Github11111111111111 <ralph.stjohniii@g.austincc.edu>
2024-09-29 04:53:17 +0000

    homework 4

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,5 @@
 import unittest
-'''
-the file in /tests/homework/b_in_proc_out/tests_in_proc_out
-has the test functions
-'''
-from tests.homework.d_repetition import tests_repetition
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_repetition)
-unittest.TextTestRunner(verbosity=2).run(suite)
+from tests.homework.e_functions import tests_functions
+
+suite = unittest.TestLoader().loadTestsFromModule(tests_functions)
--- tests/homework/e_functions/tests_functions.py
+++ tests/homework/e_functions/tests_functions.py
@@ -1,1 +1,20 @@
-#
+import unittest
+from src.homework.e_functions.value_return import get_hour, get_hours
+from src.homework.e_functions.value_return import get_minutes
+from src.homework.e_functions.value_return import get_seconds
+
+
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_hour_epoch(self):
+        self.assertEqual(23, get_hours(1727585298))
+      
+
+    def test_get_minutes_epoch(self):
+        self.assertEqual(23, get_minutes(1727585298))
+       
+
+    def test_get_seconds_epoch(self):
+        self.assertEqual(23, get_seconds(1727585298))
+        
