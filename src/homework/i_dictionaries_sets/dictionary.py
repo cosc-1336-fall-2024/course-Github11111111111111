000a6265ff3cdaf52ac40f8a47dd0b82d116336f
Github11111111111111 <ralph.stjohniii@g.austincc.edu>
2024-10-27 23:18:50 +0000

    homework 6

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,38 @@
-#
+#homework 6
+def get_p_distance(list1, list2):
+    differences = 0
+    length = len(list1)  
+
+    for i in range(length):
+        if list1[i] != list2[i]:
+            differences += 1
+
+    return differences / length
+
+def get_p_distance_matrix(lists):
+    n = len(lists)  
+    matrix = [] 
+
+    for i in range(n):
+        row = []  
+        for j in range(n):
+            distance = get_p_distance(lists[i], lists[j])
+            row.append(distance)  
+        matrix.append(row)  
+
+    return matrix
+
+# Sample input
+lists = [
+    ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A'],  # list1
+    ['G', 'A', 'T', 'T', 'C', 'A', 'T', 'T', 'T', 'C'],  # list2
+    ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'T'],  # list3
+    ['G', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A']   # list4
+]
+
+matrix = get_p_distance_matrix(lists)
+
+for row in matrix:
+    for value in row:
+        print("%.5f" % value, end=" ")  
+    print()  
--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,48 @@
-#
+#homework 6
+from dictionary import get_p_distance_matrix
+
+def get_p_distance(list1, list2):
+    differences = 0
+    length = len(list1)
+    for i in range(length):
+        if list1[i] != list2[i]:
+            differences += 1
+    return differences / length
+
+def get_p_distance_matrix(lists):
+    matrix = []
+    for i in range(len(lists)):
+        row = []
+        for j in range(len(lists)):
+            row.append(get_p_distance(lists[i], lists[j]))
+        matrix.append(row)
+    return matrix
+
+def display_matrix(matrix):
+    for row in matrix:
+        for value in row:
+            print(f"{value:.5f}", end=" ")
+        print()
+
+def main():
+    while True:
+        print("\n1- Get p distance matrix\n2- Exit")
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            n = int(input("Number of DNA sequences: "))
+            length = int(input("Length of each sequence: "))
+            dna_list = []
+
+            for i in range(n):
+                sequence = list(input(f"Sequence {i + 1}: ").strip().upper())
+                dna_list.append(sequence)
+
+            matrix = get_p_distance_matrix(dna_list)
+            print("\nP-Distance Matrix:")
+            display_matrix(matrix)
+
+        elif choice == "2":
+            print("Exit")
+            break
+main()
